bed11b272998df9cf23d8fd0a23e9365737eec1f
labohkip81 <labankiplagat81@gmail.com>
2019-05-26 18:10:24 +0300

    Used url_for function instead of the default url definition

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,21 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
 
 
-@app.route('/login')
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
 
 def login():
     form = LoginForm()
-    return render_template('login.html', title = 'Sign in', form = form)
+    if form.validate_on_submit():
+        flash('You have successfully logged in; Welcome{}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
 
 #Defines the route for home page for this application.
