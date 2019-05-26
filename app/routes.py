87be2491df4930047306ee9fa9ae9aeb8b8241c0
labohkip81 <labankiplagat81@gmail.com>
2019-05-26 17:29:33 +0300

    Login page design and form handling in Flask.

--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,40 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
+
+
+@app.route('/login')
+
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Sign in', form = form)
+
+
+#Defines the route for home page for this application.
 @app.route('/')
 @app.route('/index')
 
 
 def index():
-    user = {'username': 'Miguel'}
+    user = {'username': 'Laban'}
     posts = [
         {
             'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
+            'body': 'Beautiful day in Portland!',
+            'greeting': 'Hey there thanks for the information'
         },
 
          {
             'author': {'username': 'Laban'},
-            'body': 'Its another log here'
+            'body': 'Its another log here',
+            'greeting': 'Hey there thanks much'
         },
      
         {   
             'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
+            'body': 'The Avengers movie was so cool!',
+            'greeting': 'Woow, can\'t resist this'
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
